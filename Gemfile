9e1ceeb0fc6c08ab4592e62f1f598a908b724e6a
samrudhisharma <samrudhi.s21@gmail.com>
2015-09-23 20:40:54 -0400

    Edit information possible

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'bcrypt', '~> 3.1.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 # Use mysql as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923103022) do
+ActiveRecord::Schema.define(version: 20150924000939) do
+
+  create_table "user1s", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                  limit: 255
@@ -21,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150923103022) do
     t.string   "password_confirmation", limit: 255
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.string   "password_digest",       limit: 255
   end
 
 end
